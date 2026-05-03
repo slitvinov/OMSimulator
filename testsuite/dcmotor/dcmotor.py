dbd5f8d3982d85cae73479982cf9bb8ea5c754c7
Sergey Litvinov <slitvinov@g.harvard.edu>
2026-05-02 20:07:56 -0400

    testsuite: regenerate FMUs and disable dcmotor on macOS
    - testsuite/resources/*.mos: drop 'platforms=...' so omc generates FMUs
      for the host (darwin64 on macOS arm64); upstream uses Docker cross-build
      for linux64+win64 binaries which is unavailable on macOS runners.
    - testsuite/resources/*: regenerate with MSL 4.0.0; bundled SSPs
      refreshed with the regenerated FMUs.
    - testsuite/dcmotor/{dcmotor,dcmotorMETest}.py: '## mac: no' since the
      bundled stimuli_model.fmu is a Dymola source FMU with linux64+win64
      binaries only and depends on Dymola runtime headers we cannot ship.
    - testsuite/CompositeModels/Makefile: TEST = ../rtest (was ../../rtest,
      upstream typo).

--- testsuite/dcmotor/dcmotor.py
+++ testsuite/dcmotor/dcmotor.py
@@ -3,7 +3,7 @@
 ## linux: yes
 ## ucrt64: yes
 ## win: yes
-## mac: yes
+## mac: no
 
 ## This example demonstrates how to export an SSD file from an SSP file.
 ## the main purpose of this example is to parse all the elements in the SSD file
--- testsuite/dcmotor/dcmotorMETest.py
+++ testsuite/dcmotor/dcmotorMETest.py
@@ -3,7 +3,7 @@
 ## linux: yes
 ## ucrt64: yes
 ## win: yes
-## mac: yes
+## mac: no
 
 ## This example test dc motors fmu's with ME solver
 
